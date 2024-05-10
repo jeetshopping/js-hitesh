d0806c78091502b14935adeff9981a03f24ef83e
jeetshopping <159878495+jeetshopping@users.noreply.github.com>
2024-05-10 11:36:15 +0000

    done

---
+++ 01_basics/04_comparters.js
@@ -0,0 +1,14 @@
+console.log(null>0);
+console.log(null==0);
+console.log(null>=0);
+
+console.log(undefined>0);
+console.log(undefined==0);
+console.log(undefined>=0);
+
+// ===  this will check value and data type also 
+
+console.log("2" === 2);
+
+console.log("2">1);
+console.log("02">1);
