ce505e02c6dcc91a1c6bdc3c89abe20d1d1c3e04
jeetshopping <159878495+jeetshopping@users.noreply.github.com>
2024-06-08 12:44:38 +0000

    switch statement in js

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,51 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("Febuary");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 8:
+        console.log("Augest");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("Octember");
+        break;
+    case 11:
+        console.log("November");
+        break;
+                                                                                                                                                                                        
+
+    default:
+        console.log("December");
+        break;
+}
